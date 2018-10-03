4ce5f2cd9f1b6cd444596ca1795fe30f7b4740d1
Joslyn Pereira <31210814+VapeNationZ@users.noreply.github.com>
2018-10-03 07:02:59 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+
+
+include("auth.php"); //include auth.php file on all secure pages ?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Welcome Home</title>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+<div class="navbar">
+  <a href="#home">Home</a>
+  <a href="#news">News</a>
+  <div class="dropdown">
+    <button class="dropbtn">Dropdown 
+ 
+    </button>
+    <div class="dropdown-content">
+      <a href="#">Link 1</a>
+      <a href="#">Link 2</a>
+      <a href="#">Link 3</a>
+    </div>
+  </div> 
+</div>
+
+
+
+
+
+<br /><br /><br /><br />
+
+
+</body>
+</html>
