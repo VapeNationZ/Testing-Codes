9f89b87036a5f879731dae1864b0a63bcd4f8c36
Joslyn Pereira <31210814+VapeNationZ@users.noreply.github.com>
2019-02-09 22:39:26 +0530

    Add files via upload

--- blog.sql
+++ blog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 19, 2018 at 02:10 PM
+-- Generation Time: Oct 30, 2018 at 10:43 AM
 -- Server version: 10.1.34-MariaDB
 -- PHP Version: 5.6.37
 
@@ -41,8 +41,11 @@ CREATE TABLE `blog` (
 --
 
 INSERT INTO `blog` (`id`, `title`, `content`, `aurthor_name`, `date`) VALUES
-(1, 'Test', 'this test', 'tester', '2018-10-17 10:06:01'),
-(2, 'test 2', 'andjb', 'susahnt', '2018-10-17 10:43:10');
+(1, 'Test', 'this test', 'tester', '2018-10-29 10:17:44'),
+(2, 'test 2', 'andjb', 'susahnt', '2018-10-29 10:17:54'),
+(3, 'Testing', '<p><s>asc</s></p>', 'Hero Alam', '2018-10-29 10:18:46'),
+(4, 'Again testing', 'dbvbv', 'Hero Ala,m', '2018-10-29 10:19:17'),
+(5, 'Try with like', '<p><strong>This for test for Like</strong></p>', 'Tester', '2018-10-29 13:40:24');
 
 -- --------------------------------------------------------
 
@@ -69,6 +72,52 @@ INSERT INTO `comments` (`comment_id`, `comment_blog_id`, `author_name`, `author_
 (3, 1, 'sdv', 'fv', 'dsv', '2018-10-18 11:31:43'),
 (4, 2, 'test', 'test@gmail.com', 'svkjb', '2018-10-18 11:33:38');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `likes`
+--
+
+CREATE TABLE `likes` (
+  `id` int(11) NOT NULL,
+  `blog_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `likes` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `likes`
+--
+
+INSERT INTO `likes` (`id`, `blog_id`, `user_id`, `likes`) VALUES
+(44, 4, 1, 1),
+(50, 1, 2, 1),
+(72, 1, 1, 1),
+(73, 2, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `uid` int(11) NOT NULL,
+  `fname` varchar(20) NOT NULL,
+  `lname` varchar(20) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `phone` varchar(13) NOT NULL,
+  `dob` varchar(15) NOT NULL,
+  `password` varchar(9999) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`uid`, `fname`, `lname`, `email`, `phone`, `dob`, `password`) VALUES
+(1, 'Purvesh', 'Jain', 'jainpurvesh19@gmail.com', '9987795559', '19-04-1998', 'password');
+
 --
 -- Indexes for dumped tables
 --
@@ -86,6 +135,18 @@ ALTER TABLE `comments`
   ADD PRIMARY KEY (`comment_id`);
 
 --
+-- Indexes for table `likes`
+--
+ALTER TABLE `likes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`uid`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -93,13 +154,25 @@ ALTER TABLE `comments`
 -- AUTO_INCREMENT for table `blog`
 --
 ALTER TABLE `blog`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
   MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `likes`
+--
+ALTER TABLE `likes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=74;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
