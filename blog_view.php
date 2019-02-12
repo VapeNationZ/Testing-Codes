8e2ba81c9b674bf3fd47f90674e89d39f0773901
Joslyn Pereira <31210814+VapeNationZ@users.noreply.github.com>
2019-02-12 19:00:26 +0530

    Add files via upload

--- blog_view.php
+++ blog_view.php
@@ -1,3 +1,5 @@
+<?php session_start(); ob_start(); 
+$uid = $_SESSION['id'];?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,65 +8,75 @@
 	<title>Retrive</title>
 </head>
 <link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <style>
-	body {
-		margin-left: 50px;
-		margin-right: 50px;
+	i.fa.fa-heart {
+		color: red;
+		font-size: 25px;
 	}
+
+	i.fa.fa-heart-o {
+		color: black;
+		font-size: 25px;
+	}
+	
+	.count{
+		margin-left: 10px;
+		color: red;
+		font-size: 25px;
+	}
+	
+	
+	
 
 </style>
 
 <body>
+	<h1 class="text-center my-5">
+		Posts
 
+	</h1>
 	<div class="container">
+
 		<div class="row">
-			<h1 class="text-center">
-				Posts
-
-			</h1>
-			<hr>
 			<!--First Blog Post -->
-
-
-			<?php
-
-include("includes/db.php");
-include("includes/function.php");
-
-$query = "select * from blog";
-$result = mysqli_query($conn,$query);
-
-while($row = mysqli_fetch_array($result)){
-	$blog_id = $row['id'];
-	$blog_title = $row['title'];
-	$blog_author = $row['aurthor_name'];
-	$blog_date = $row['date'];
-	$blog_content = $row['content'];
-?>
-			<h2>
-				<a href="blog_comment.php?b_id=<?php echo $blog_id ?>"><?php echo $blog_title; ?></a>
-			</h2>
-			<p class="lead">
-				by 
-					<?php echo $blog_author; ?>
-			</p>
-			<p><span class="fa fa-clock-o"></span>
-				<?php 
-	$blog_date = date('d/m/Y ', strtotime($blog_date));
-	echo $blog_date ;?>
-			</p>
-
-
-
-			<p>
-				<?php echo $blog_content;?>
-			</p>
-			<hr>
+			<br><br><br><br>
+			<div id="blog">
+				
+			</div>
 		</div>
+
 	</div>
-	<?php }?>
-	<script src="js/bootstrap.min.js"></script>
-	<script src="js/jquery-3.2.1.min.js"></script>
+
+
+	<script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+	<script>
+		$(document).ready(function() {
+
+			setInterval(function() {
+				updateBlogs();
+			}, 500);
+
+
+			function updateBlogs() {
+				
+				$.ajax({
+					url: 'updateBlogs.php',
+					type: 'POST',
+					success: function(data) {
+						if (!data.error) {
+							$('#blog').html(data);
+						}
+					}
+				});
+			}
+
+		});
+
+	</script>
+
+	
+
 </body>
 
 </html>
